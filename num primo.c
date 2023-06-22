9757b0ec5f4a262b849f5bf57e5911296dec30b7
EliassRobless <136567059+EliassRobless@users.noreply.github.com>
2023-06-22 10:24:34 -0600

    Add files via upload

---
+++ num primo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int numPrimo(int numero),i;
+
+int main(void) {
+  int numero;
+  printf("Escribe un numero entero positivo: \n");
+  scanf("%d", &numero);
+  if (numPrimo(numero)) {
+    printf("Es un numero primo");
+  } else {
+    printf("No es un numero primo");
+  }
+  return 0;
+}
+
+int numPrimo(int numero) {
+  if (numero == 0 || numero == 1) return 0;
+
+  if (numero == 4) return 0;
+  for (i = 2; i < numero / 2; i++) {
+
+    if (numero % i == 0) return 0;
+  }
+
+  return 1;
+}
